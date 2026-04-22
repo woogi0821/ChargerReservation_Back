0bb2e8bdc9517c501364b32b99fd28e83c229d10
sunterx1-afk <sunterx1@gmail.com>
2026-04-22 14:03:37 +0900

    2026/04/22 admin오류 슈정
    2026/04/22 admin오류 슈정

--- Backend/chargerReservation/src/main/java/com/simplecoding/chargerreservation/admin/entity/Admin.java
+++ Backend/chargerReservation/src/main/java/com/simplecoding/chargerreservation/admin/entity/Admin.java
@@ -12,7 +12,14 @@ import lombok.NoArgsConstructor;
 public class Admin extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    // 1. 전략은 유지하되, 사용할 generator 이름을 지정합니다.
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "admin_seq_gen")
+    // 2. 상세 시퀀스 설정을 추가합니다.
+    @SequenceGenerator(
+            name = "admin_seq_gen",
+            sequenceName = "ADMIN_SEQ", // 어제 팀원이 DB에 만든 시퀀스 이름
+            allocationSize = 1          // 핵심! DB의 INCREMENT BY 1과 맞춰줍니다.
+    )
     @Column(name = "ADMIN_ID")
     private Long adminId;
 
