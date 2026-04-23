30d4d8655fca12259bf736ad889f875288a8feb2
blackpoint2-code <blackpoint2@naver.com>
2026-04-23 14:16:18 +0900

    4/23 작업
    4/23 작업

--- Backend/chargerReservation/src/main/java/com/simplecoding/chargerreservation/notification/sse/SseController.java
+++ Backend/chargerReservation/src/main/java/com/simplecoding/chargerreservation/notification/sse/SseController.java
@@ -2,14 +2,16 @@ package com.simplecoding.chargerreservation.notification.sse;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/notifications")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173") // 👈 리액트 주소 허용
 public class SseController {
     private final SseEmitters sseEmitters;
 
